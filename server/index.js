194ff1313d6e6e04b7f538aac94a80290814a8a6
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-04-13 09:05:58 -0600

    friday-morning

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session');
+require('dotenv').config();
+const createInitialSession = require('./middlewares/session.js')
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 10000
+    }
+}))
+app.use(createInitialSession);
 
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
