12bfc4e56cdddfe528e0b9a6f561a052185d6982
Skyler Sorensen <skylersorensen@Skylers-MacBook-Air.local>
2018-04-14 11:06:19 -0600

    finished

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const mc = require( `./controllers/messages_controller` );
 const session = require('express-session');
 require('dotenv').config();
 const createInitialSession = require('./middlewares/session.js')
+const filter = require('./middlewares/filter');
 
 const app = express();
 
@@ -17,12 +18,18 @@ app.use(session({
         maxAge: 10000
     }
 }))
-app.use(createInitialSession);
+app.use(createInitialSession);//from middleware/session - to check to see if they are already a user
+app.use((req, res, next) => { //from middleware/filter - to filter out bad words established on the other file
+    if (req.method === 'POST' || req.method === 'PUT'){
+        filter(req, res, next);
+    } else {next();}
+})
 
 app.post( "/api/messages", mc.create );
 app.get( "/api/messages", mc.read );
 app.put( "/api/messages", mc.update );
 app.delete( "/api/messages", mc.delete );
+app.get('api/messages/history', mc.history);
 
 const port = process.env.PORT || 3000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
